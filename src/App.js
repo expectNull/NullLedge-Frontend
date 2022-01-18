adc3dc33e4cb91924baa13e8efa1fd6f867cd4ce
Hyunsoo <hyunsoo99kim@gmail.com>
2022-01-19 02:41:37 +0900

    modify : router add routes for new components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import LandingPage from './components/views/LandingPage/LandingPage';
 import LoginPage from './components/views/LoginPage/LoginPage';
 import Navbar from './components/views/Navbar/Navbar';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
+import MyAnswer from './components/views/MyAnswer/MyAnswer';
+import MyQuestion from './components/views/MyQuestions/MyQuestions';
+import Ranking from './components/views/Ranking/Ranking';
 
 function App() {
   return (
@@ -17,6 +20,11 @@ function App() {
             <Route exact path="/" element={<LandingPage />} />
             <Route exact path="/login" element={<LoginPage />} />
             <Route exact path="/register" element={<RegisterPage />} />
+            <Route exact path="/myanswer" element={<MyAnswer />} />
+            <Route exact path="/myanswer/:userId" element={<MyAnswer />} />
+            <Route exact path="/myquestions" element={<MyQuestion />} />
+            <Route exact path="/myquestions/:userId" element={<MyQuestion />} />
+            <Route exact path="/ranking" element={<Ranking />} />
           </Routes>
         </div>
       </Router>
