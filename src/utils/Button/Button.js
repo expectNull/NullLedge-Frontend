8552a717f4796edaa22ed7fcf920bf65a8f790fb
ShinHyoung <jsin2475@naver.com>
2022-01-21 20:40:07 +0900

    add : many buttons
    버튼의 경우 내부 TEXT에 스페이스가 존재할 경우 창이 작아지면
    [ -> 방향으로 그려지던 것이 아래 방향으로 가게됨, disableElevation이란 attribute가 적용이 안되서 글을 바꿔둠]

---
+++ src/utils/Button/Button.js
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+
+const theme = createTheme({
+  palette: {
+    neutral: {
+      main: '#fff',
+      contrastText: '#000',
+    },
+    upload: {
+      main: '#5999FF',
+      contrastText: '#000',
+    },
+  },
+});
+
+function handleLogin() {}
+
+function handleLogout() {}
+
+function handleAsk() {}
+
+function handleUpload() {}
+
+export function LoginButton() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Button color="neutral" variant="outlined" onClick={handleLogin}>
+        Login
+      </Button>
+    </ThemeProvider>
+  );
+}
+
+export function LogoutButton() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Button color="neutral" variant="outlined" onClick={handleLogout}>
+        Logout
+      </Button>
+    </ThemeProvider>
+  );
+}
+
+export function AskButton() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Button color="upload" variant="outlined" onClick={handleAsk}>
+        AskQuestion
+      </Button>
+    </ThemeProvider>
+  );
+}
+
+export function UploadButton() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Button color="upload" variant="outlined" onClick={handleUpload}>
+        Upload
+      </Button>
+    </ThemeProvider>
+  );
+}
