04b46c8b54ca3dd8218d0264c4b82b9df18bb9eb
ShinHyoung <jsin2475@naver.com>
2022-01-22 00:32:55 +0900

    modify : this file only routes the page
    충돌이 생겨서 이 파일은 다른 페이지들을 찾기만 함

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Footer from './components/views/Footer/Footer';
-import LandingPage from './components/views/LandingPage/LandingPage';
-import LoginPage from './components/views/LoginPage/LoginPage';
-import Navbar from './components/views/Navbar/Navbar';
-import RegisterPage from './components/views/RegisterPage/RegisterPage';
-import MyAnswer from './components/views/MyAnswer/MyAnswer';
-import MyQuestion from './components/views/MyQuestions/MyQuestions';
-import Ranking from './components/views/Ranking/Ranking';
+import LandingPage from './Components/views/LandingPage/LandingPage';
+import LoginPage from './Components/views/LoginPage/LoginPage';
+import RegisterPage from './Components/views/RegisterPage/RegisterPage';
+import MyAnswer from './Components/views/MyAnswer/MyAnswer';
+import MyQuestion from './Components/views/MyQuestions/MyQuestions';
+import Ranking from './Components/views/Ranking/Ranking';
 
 function App() {
   return (
-    <div className="App">
-      <Footer />
-      <Router>
-        <Navbar />
-        <div>
-          <Routes>
-            <Route exact path="/" element={<LandingPage />} />
-            <Route exact path="/login" element={<LoginPage />} />
-            <Route exact path="/register" element={<RegisterPage />} />
-            <Route exact path="/myanswer" element={<MyAnswer />} />
-            <Route exact path="/myanswer/:userId" element={<MyAnswer />} />
-            <Route exact path="/myquestions" element={<MyQuestion />} />
-            <Route exact path="/myquestions/:userId" element={<MyQuestion />} />
-            <Route exact path="/ranking" element={<Ranking />} />
-          </Routes>
-        </div>
-      </Router>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path="/register" element={<RegisterPage />} />
+        <Route exact path="/myanswer" element={<MyAnswer />} />
+        <Route exact path="/myanswer/:userId" element={<MyAnswer />} />
+        <Route exact path="/myquestions" element={<MyQuestion />} />
+        <Route exact path="/myquestions/:userId" element={<MyQuestion />} />
+        <Route exact path="/ranking" element={<Ranking />} />
+      </Routes>
+    </Router>
   );
 }
 
