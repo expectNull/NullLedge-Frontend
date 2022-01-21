5d3c8d958f7edcd56343dd9afbf6f2c088cd9a4d
ShinHyoung <jsin2475@naver.com>
2022-01-22 00:38:26 +0900

    modify : MainPage with Header
    class "questions" : 질문 카드 유틸을 만들어 내부에 추가하는 방식
    
    class "footer" : questions 페이지 이동을 위해 쪽수, 카테고리 표시를 위한 컨테이너.

--- src/Components/views/LandingPage/LandingPage.js
+++ src/Components/views/LandingPage/LandingPage.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import './LandingPage.css';
+import Header from '../../../utils/Header/Header';
+import Navbar from '../../../utils/Navbar/Navbar';
+import Footer from '../../../utils/Footer/Footer';
 
 function LandingPage(props) {
-  return <div>LandingPage 랜딩 페이지</div>;
+  return (
+    <div className="page">
+      <Navbar />
+      <div className="center">
+        <div className="header">
+          <Header />
+        </div>
+
+        <div className="questions"></div>
+
+        <div className="footer"></div>
+      </div>
+
+      <Footer />
+    </div>
+  );
 }
 
 export default LandingPage;
