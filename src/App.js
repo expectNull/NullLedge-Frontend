18735beb2d48f9c5ecff26cb7c36e3e4860eb2c7
Hyunsoo <hyunsoo99kim@gmail.com>
2022-01-25 15:01:51 +0900

    modify : folder name

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import LandingPage from './Components/views/LandingPage/LandingPage';
-import LoginPage from './Components/views/LoginPage/LoginPage';
-import RegisterPage from './Components/views/RegisterPage/RegisterPage';
-import MyAnswer from './Components/views/MyAnswer/MyAnswer';
-import MyQuestion from './Components/views/MyQuestions/MyQuestions';
-import Ranking from './Components/views/Ranking/Ranking';
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+import MyAnswer from './components/views/MyAnswer/MyAnswer';
+import MyQuestion from './components/views/MyQuestions/MyQuestions';
+import Ranking from './components/views/Ranking/Ranking';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<LandingPage />} />
-        <Route exact path="/login" element={<LoginPage />} />
-        <Route exact path="/register" element={<RegisterPage />} />
-        <Route exact path="/myanswer" element={<MyAnswer />} />
-        <Route exact path="/myanswer/:userId" element={<MyAnswer />} />
-        <Route exact path="/myquestions" element={<MyQuestion />} />
-        <Route exact path="/myquestions/:userId" element={<MyQuestion />} />
-        <Route exact path="/ranking" element={<Ranking />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/login" element={<LoginPage />} />
+          <Route exact path="/register" element={<RegisterPage />} />
+          <Route exact path="/myanswer" element={<MyAnswer />} />
+          <Route exact path="/myanswer/:userId" element={<MyAnswer />} />
+          <Route exact path="/myquestions" element={<MyQuestion />} />
+          <Route exact path="/myquestions/:userId" element={<MyQuestion />} />
+          <Route exact path="/ranking" element={<Ranking />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
