2da62b24802ed8b169bdff49539d7743ec1c23bb
ShinHyoung <jsin2475@naver.com>
2022-02-14 13:34:52 +0900

    [add] utils

--- src/utils/Header/Header.js
+++ src/utils/Header/Header.js
@@ -1,13 +1,57 @@
-import './Header.css';
-import { AskButton } from '../Button/Button';
+import './Navbar.css';
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import { AppBar, Tabs, Tab } from '@material-ui/core';
+import HomeIcon from '@mui/icons-material/Home';
+import Toolbar from '@mui/material/Toolbar';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import QuizIcon from '@mui/icons-material/Quiz';
+import ChatIcon from '@mui/icons-material/Chat';
+import SearchAppBar from '../Search/Search';
+import { LoginButton, LogoutButton } from '../Button/Button';
 
-export default function Header(props) {
+function Navbar(props) {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
-    <>
-      <span className="header_tag">All Question</span>
-      <div className="header_btn">
-        <AskButton />
+    <AppBar position="static" color="primary">
+      <div className="nav">
+        <Toolbar>
+          <Tabs value={value} onChange={handleChange} aria-label="Main Tabs">
+            <Tab label="Home" icon={<HomeIcon />} to="/" component={Link} />
+            <Tab
+              label="My Questions"
+              icon={<QuizIcon />}
+              to="/myquestions"
+              component={Link}
+            />
+            <Tab
+              label="My Answers"
+              icon={<HomeIcon />}
+              to="/myanswer"
+              component={Link}
+            />
+            <Tab
+              label="Ranking"
+              icon={<ThumbUpIcon />}
+              to="/ranking"
+              component={Link}
+            />
+          </Tabs>
+        </Toolbar>
+
+        <div className="nav_btns">
+          <SearchAppBar />
+          <div className="nav_btn">
+            <LoginButton />
+          </div>
+        </div>
       </div>
-    </>
+    </AppBar>
   );
 }
+
+export default Navbar;
