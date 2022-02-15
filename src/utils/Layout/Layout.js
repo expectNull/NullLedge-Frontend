c119dacf494cd80c6273acf717f8325449ae1fc9
ShinHyoung <jsin2475@naver.com>
2022-02-15 17:55:55 +0900

    modify : remove codes

--- src/utils/Layout/Layout.js
+++ src/utils/Layout/Layout.js
@@ -4,23 +4,11 @@ import React from 'react';
 import { Box } from '@material-ui/core';
 import Nav from '../Nav/Nav';
 
-export function Layout({ children }) {
-  return (
-    <>
-      <Box
-        className="layout_box"
-        sx={{
-          display: 'flex',
-        }}
-      >
-        <Nav />
-        {children}
-      </Box>
-    </>
-  );
-}
+import { UserCard } from '../UserCard/UserCard';
 
-export function RankLayout({ children }) {
+export function Layout({ children }) {
+  const page = window.location.href.split('/')[3];
+
   return (
     <>
       <Box
@@ -30,19 +18,24 @@ export function RankLayout({ children }) {
         }}
       >
         <Nav />
-        <table className="rank_table">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>이름</th>
-              <th>상태 메세지</th>
-              <th>NP</th>
-              <th>Reply</th>
-              <th>Ask</th>
-            </tr>
-          </thead>
-          <tbody>{children}</tbody>
-        </table>
+        {page === 'ranking' ? (
+          <table className="rank_table">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>이름</th>
+                <th>상태 메세지</th>
+                <th>NP</th>
+                <th>Reply</th>
+                <th>Ask</th>
+              </tr>
+            </thead>
+            <tbody>{children}</tbody>
+          </table>
+        ) : (
+          <>{children}</>
+        )}
+        <UserCard name="Jang" postIdx={3} />
       </Box>
     </>
   );
