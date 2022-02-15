feccb1197f3661ffcff44ffee570d3f61e66c207
ShinHyoung <jsin2475@naver.com>
2022-02-16 00:44:15 +0900

    modify : append div to spans

--- src/utils/UserCard/UserCard.js
+++ src/utils/UserCard/UserCard.js
@@ -2,14 +2,17 @@ import './UserCard.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+// <UserCard name="Jang" postIdx={3} />
 export function UserCard({ name, postIdx }) {
   // postIdx를 통해 쿼리 던지기. => 질문의 날짜, 시간을 가져옴, 질문자 답변자 구분 필요
-  const date = window.Date.now();
+  const date = window.Date.now.;
   const page = window.location.href.split('/')[3];
 
   return (
     <div className="user_card">
-      <div className="user_date">{date}</div>
+      <div className="user_date">
+        <span className="span_date">{date}</span>
+      </div>
       <div className="user_container">
         <Link to={`/mypage/${name}`}>
           <img
@@ -18,7 +21,12 @@ export function UserCard({ name, postIdx }) {
           />
         </Link>
         <div className="user_info">
-          <Link to={`/mypage/${name}`}>{name}</Link>
+          <Link
+            to={`/mypage/${name}`}
+            style={{ color: 'blue', textDecoration: 'none' }}
+          >
+            {name}
+          </Link>
           <span title="NP">{postIdx}</span>
           <span title="Answers">{postIdx}</span>
         </div>
