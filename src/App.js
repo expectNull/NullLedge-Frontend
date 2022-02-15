5013f8bd8997c8b4b5af2c6cf19ade40f9ff0f43
Hyunsoo <hyunsoo99kim@gmail.com>
2022-02-15 15:04:56 +0900

    add : Header, Footer 기본 페이지 Header, Footer 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import { BrowserRouter as BRouter } from 'react-router-dom';
 import { Router } from './Router';
+import Header from './utils/Header/Header';
+import Footer from './utils/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <BRouter>
         <Router />
       </BRouter>
+      <Footer />
     </div>
   );
 }
