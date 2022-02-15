2bdf7115783c04d17d1a56d485982e2644663121
ShinHyoung <jsin2475@naver.com>
2022-02-15 17:57:02 +0900

    modify : it need to be css design

---
+++ src/utils/UserCard/UserCard.js
@@ -0,0 +1,28 @@
+import './UserCard.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export function UserCard({ name, postIdx }) {
+  // postIdx를 통해 쿼리 던지기. => 질문의 날짜, 시간을 가져옴, 질문자 답변자 구분 필요
+  const date = window.Date.now();
+  const page = window.location.href.split('/')[3];
+
+  return (
+    <div className="user_card">
+      <div className="user_date">{date}</div>
+      <div className="user_container">
+        <Link to={`/mypage/${name}`}>
+          <img
+            className="user_pic"
+            src="https://i.stack.imgur.com/6OJea.jpg?s=64&g=1"
+          />
+        </Link>
+        <div className="user_info">
+          <Link to={`/mypage/${name}`}>{name}</Link>
+          <span title="NP">{postIdx}</span>
+          <span title="Answers">{postIdx}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
