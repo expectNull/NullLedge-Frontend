5f1d223acf314ee2869e9d6aafc475ca5b8ab023
ShinHyoung <jsin2475@naver.com>
2022-02-15 17:57:36 +0900

    modify : button is added

--- src/utils/Rating/Rating.js
+++ src/utils/Rating/Rating.js
@@ -1,25 +1,30 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
 
-export default function BasicRating() {
-  const [value, setValue] = React.useState(2.5);
+export function BasicRating() {
+  // 쿼리로 해당 질문에 Rate 되었는지 확인 필요.
+  const [value, setValue] = useState(0);
+  const [check, setCheck] = useState(false);
+
+  function isValue(event, newValue) {
+    setValue(newValue);
+  }
+
+  function submit() {
+    // 쿼리로 점수 전달하기.
+    setCheck(check => !check);
+  }
 
   return (
-    <Box
-      sx={{
-        '& > legend': { mt: 2 },
-      }}
-    >
-      <Typography component="legend">Controlled</Typography>
-      <Rating
-        name="simple-controlled"
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      />
+    <Box>
+      <Typography component="legend">Rate the reply</Typography>
+      <Rating name="simple-controlled" value={value} onChange={isValue} />
+      <br />
+      <button onClick={submit} disabled={check}>
+        제출
+      </button>
     </Box>
   );
 }
