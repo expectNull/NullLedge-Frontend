b6dc281b806016672fddd10a4338cb2e350b2a17
ShinHyoung <jsin2475@naver.com>
2022-02-15 15:27:22 +0900

    modify : remove all header, footer

--- src/utils/Layout/Layout.js
+++ src/utils/Layout/Layout.js
@@ -2,18 +2,11 @@ import './Layout.css';
 import React from 'react';
 
 import { Box } from '@material-ui/core';
-
-import useWindowSize from '../WindowSize';
 import Nav from '../Nav/Nav';
 
 export function Layout({ children }) {
-  const { width, height } = useWindowSize();
-  console.log(width, height);
-
   return (
     <>
-      <Box sx={{ display: 'grid' }}>Header</Box>
-
       <Box
         sx={{
           display: 'flex',
@@ -22,8 +15,6 @@ export function Layout({ children }) {
         <Nav />
         {children}
       </Box>
-
-      <Box sx={{ display: 'grid' }}>Footer</Box>
     </>
   );
 }
@@ -31,22 +22,26 @@ export function Layout({ children }) {
 export function RankLayout({ children }) {
   return (
     <>
-      <Box sx={{ display: 'grid' }}>Header</Box>
-      <table className="rank_table">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>이름</th>
-            <th>상태 메세지</th>
-            <th>NP</th>
-            <th>Reply</th>
-            <th>Ask</th>
-          </tr>
-        </thead>
-        <tbody>{children}</tbody>
-      </table>
-      <div className="page">page</div>
-      <Box sx={{ display: 'grid' }}>Footer</Box>
+      <Box
+        sx={{
+          display: 'flex',
+        }}
+      >
+        <Nav />
+        <table className="rank_table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>이름</th>
+              <th>상태 메세지</th>
+              <th>NP</th>
+              <th>Reply</th>
+              <th>Ask</th>
+            </tr>
+          </thead>
+          <tbody>{children}</tbody>
+        </table>
+      </Box>
     </>
   );
 }
