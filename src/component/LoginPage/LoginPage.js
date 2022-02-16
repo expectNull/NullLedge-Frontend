78aadf12aa3ec679e90c55d33f61a9358e095af3
Hyunsoo <hyunsoo99kim@gmail.com>
2022-02-17 02:01:42 +0900

    modify : setting Post connect with BE about writing post

--- src/component/LoginPage/LoginPage.js
+++ src/component/LoginPage/LoginPage.js
@@ -2,9 +2,7 @@ import React from 'react';
 import Header from '../../utils/Header/Header';
 
 function LoginPage(props) {
-  return <div className="page">
-    gjgjgjg
-  </div>;
+  return <div className="page">gjgjgjg</div>;
 }
 
 export default LoginPage;
