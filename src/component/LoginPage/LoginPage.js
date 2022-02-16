bf8f7c698b1bcec33e2b6ce1a4c9ab98cad97afa
박병휘 <201802093@o.cnu.ac.kr>
2022-02-16 22:52:41 +0900

    mod : gjgjg

--- src/component/LoginPage/LoginPage.js
+++ src/component/LoginPage/LoginPage.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Header from '../../utils/Header/Header';
 
 function LoginPage(props) {
-  return <div className="page">LoginPage</div>;
+  return <div className="page">
+    gjgjgjg
+  </div>;
 }
 
 export default LoginPage;
