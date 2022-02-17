1eaf5c675cccbd3904f16301adc6fcc38ae6490d
박병휘 <201802093@o.cnu.ac.kr>
2022-02-17 21:40:16 +0900

    mod : MyPage.js and .css

--- src/component/MyPage/My.js
+++ src/component/MyPage/My.js
@@ -1,18 +1,53 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { Layout } from '../../utils/Layout/Layout';
+import { PostItem } from '../../utils/PostItem/PostItem';
+import './My.css';
 
 function MyPage({ children }) {
   const { name } = useParams();
   console.log(name);
 
   return (
-    <>
-      <Layout>
-        User Slug
-        {name}
-      </Layout>
-    </>
+    <div className="MyPage">
+      <div className="helper">
+        <h1>Infomation</h1>
+        <hr></hr>
+        <table className="userInfo1">
+          <tbody>
+            <tr>
+              <td className="imgbox" rowSpan={3}>
+                <img src="userimg.png" className="userImg"></img>
+              </td>
+              <td className="nickname">{'nickname'}</td>
+            </tr>
+            <tr>
+              <td className="intro">{'intro'}</td>
+            </tr>
+            <tr>
+              <td className="nullPoint">nullPoint : {0}</td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="userInfo2">
+          <h2>My Question</h2>
+          <div className="questionList">
+            <PostItem
+              name="질문인데 이걸로 어떻게 해야함"
+              writer="Kim"
+              postIdx="5"
+            />
+          </div>
+          <h2>My Answer</h2>
+          <div className="answerList">
+            <PostItem
+              name="질문인데 이걸로 어떻게 해야함"
+              writer="Kim"
+              postIdx="5"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
