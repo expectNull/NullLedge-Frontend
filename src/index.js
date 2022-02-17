7ca1c47d6519c7c8510a09da6863dea4a6bd2a96
Hyunsoo <hyunsoo99kim@gmail.com>
2022-02-17 22:09:13 +0900

    modify : React.StrictMode 랜딩페이지 렌더 두번 하는 오류 해결

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
+// ReactDOM.render(<App />, document.getElementById('root'));
+// -> landingpage 렌더 두 번 하는 오류 해결.
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
