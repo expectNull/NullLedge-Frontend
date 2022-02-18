d972b134bce037df5c704f72319fd590e900650f
박병휘 <201802093@o.cnu.ac.kr>
2022-02-18 23:21:18 +0900

    mod : Post.js and .css

--- src/component/PostPage/Post.js
+++ src/component/PostPage/Post.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Layout } from '../../utils/Layout/Layout';
+import './Post.css';
 
 function PostPage({}) {
   return (
     <>
-      <Layout></Layout>
+      <Layout>
+        <div className="titleDiv">
+          <h1>Hi</h1>
+          <span>Asked {9} days ago</span>
+          <span>Active {6} days ago</span>
+          <span>Viewed {200} times</span>
+          <hr></hr>
+        </div>
+      </Layout>
     </>
   );
 }
