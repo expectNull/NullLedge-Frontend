9633ecc3706f55eff3596928d9b6eb396968a374
dblepart99 <minbmw8205@gmail.com>
2022-03-27 23:42:32 +0900

    setting: add setting button

--- src/component/SettingPage/Setting.js
+++ src/component/SettingPage/Setting.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import './Setting.css';
-import Button from '@mui/material/Button';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { checkCookie } from '../../utils/checkCookie';
 import { Alert, AlertDiv } from '../../utils/Alert/Alert';
+import Button from '@mui/material/Button';
+import './Setting.css';
+
 function SettingPage({}) {
+  const { name } = useParams();
+  const [info, setInfo] = useState(-1);
+  async function getInfo() {
+    const info = {
+      user: name,
+      token: await checkCookie(),
+    };
+
+    let response = await (
+      await axios.post(process.env.REACT_APP_API_URL + '/getUser', info)
+    ).data;
+    setInfo(response);
+  }
+  useEffect(() => {
+    // 유저 등록한 날짜도 추가할까요??
+    // info.user_sign에 들어있음.
+    getInfo();
+  }, []);
+
   return (
     <div className="SettingPage">
       <div className="helper">
@@ -12,12 +35,16 @@ function SettingPage({}) {
           <tbody>
             <tr>
               <td className="imgbox" rowSpan={2}>
-                <img src="userimg.png" className="userImg"></img>
+                <img
+                  src="/images/userimg.png"
+                  width="30%"
+                  className="userImg"
+                ></img>
               </td>
-              <td className="nickname">Winter</td>
+              <td className="nickname">{info.user_nm}</td>
             </tr>
             <tr>
-              <td className="intro">intro</td>
+              <td className="intro"></td>
             </tr>
           </tbody>
         </table>
@@ -26,7 +53,11 @@ function SettingPage({}) {
             <tr>
               <td>ID</td>
               <td>
-                <input className="idField" disabled value="User's ID"></input>
+                <input
+                  className="idField"
+                  disabled
+                  value={info.user_nm}
+                ></input>
               </td>
             </tr>
             <tr>
@@ -43,21 +74,53 @@ function SettingPage({}) {
               <td>
                 <input type="password" className="pwconfirmField"></input>
               </td>
+              <td>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    alert('저장되었습니다.');
+                  }}
+                >
+                  비밀번호 변경
+                </Button>
+              </td>
             </tr>
             <tr>
               <td>Nickname</td>
               <td>
                 <input className="nicknameField"></input>
               </td>
+              <td>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    alert('저장되었습니다.');
+                  }}
+                >
+                  닉네임 변경
+                </Button>
+              </td>
             </tr>
+
             <tr>
               <td>Edit Status</td>
               <td colSpan={2}>
                 <textarea rows={5} cols={45}></textarea>
               </td>
+              <td>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    alert('저장되었습니다.');
+                  }}
+                >
+                  상태 메시지 변경
+                </Button>
+              </td>
             </tr>
           </tbody>
         </table>
+<<<<<<< Updated upstream
         <Button
           variant="contained"
           color="success"
@@ -67,6 +130,8 @@ function SettingPage({}) {
         >
           Save
         </Button>
+=======
+>>>>>>> Stashed changes
       </div>
     </div>
   );
