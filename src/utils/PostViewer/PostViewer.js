6c97877060056179b5c8e5f39f66fb54b8d5c5d4
LONGNEW <shinhyoung26@gmail.com>
2022-04-01 01:04:42 +0900

    modfiy : roll back

--- src/utils/PostViewer/PostViewer.js
+++ src/utils/PostViewer/PostViewer.js
@@ -1,33 +1,35 @@
 import React from 'react';
 import './PostViewer.css';
+import { Viewer } from '@toast-ui/react-editor';
 import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 
 function PostViewer({ content }) {
-  return (
-    <ReactMarkdown
-      children={content}
-      className="MyViewer"
-      components={{
-        code({ node, inline, className, children, ...props }) {
-          const match = /language-(\w+)/.exec(className || '');
-          return !inline && match ? (
-            <SyntaxHighlighter
-              children={String(children).replace(/\n$/, '')}
-              // style={darcula}
-              language={match[1]}
-              PreTag="div"
-              {...props}
-            />
-          ) : (
-            <code className={className} {...props}>
-              {children}
-            </code>
-          );
-        },
-      }}
-    />
-  );
+  return <Viewer className="MyViewer" initialValue={content} />;
+  // return (
+  //   <ReactMarkdown
+  //     children={content}
+  //     className="MyViewer"
+  //     components={{
+  //       code({ node, inline, className, children, ...props }) {
+  //         const match = /language-(\w+)/.exec(className || '');
+  //         return !inline && match ? (
+  //           <SyntaxHighlighter
+  //             children={String(children).replace(/\n$/, '')}
+  //             // style={darcula}
+  //             language={match[1]}
+  //             PreTag="div"
+  //             {...props}
+  //           />
+  //         ) : (
+  //           <code className={className} {...props}>
+  //             {children}
+  //           </code>
+  //         );
+  //       },
+  //     }}
+  //   />
+  // );
 }
 
 export default PostViewer;
