beb52863bf934d3d96d4eb9e1758f274754b6b1c
LONGNEW <shinhyoung26@gmail.com>
2022-04-01 02:27:13 +0900

    modify : adding code highlight

--- src/utils/MyEditor/MyEditor.js
+++ src/utils/MyEditor/MyEditor.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import Prism from 'prismjs';
-import 'prismjs/themes/prism.css';
 import './MyEditor.css';
 
 import 'codemirror/lib/codemirror.css';
 import '@toast-ui/editor/dist/toastui-editor.css';
+
 import 'prismjs/components/prism-python.js';
+// import 'prismjs/themes/prism.css';
+import 'prismjs/components/prism-c.js';
+import 'prismjs/components/prism-cmake.js';
+import 'prismjs/components/prism-cpp';
+// import 'prismjs/components/prism-django.js';
+import 'prismjs/components/prism-docker.js';
+// import 'prismjs/components/prism-fortran.js';
+// import 'prismjs/components/prism-git.js';
+// import 'prismjs/components/prism-go.js';
+// import 'prismjs/components/prism-cshtml.js';
+import 'prismjs/components/prism-java.js';
+// import 'prismjs/components/prism-json.js';
+// import 'prismjs/components/prism-kotlin';
+// import 'prismjs/components/prism-log.js';
+import 'prismjs/components/prism-nginx.js';
+import 'prismjs/components/prism-ocaml.js';
+import 'prismjs/components/prism-php.js';
+// import 'prismjs/components/prism-regex.js';
+// import 'prismjs/components/prism-ruby.js';
+import 'prismjs/components/prism-sql.js';
+// import 'prismjs/components/prism-swift';
+import 'prismjs/components/prism-tsx.js';
+import 'prismjs/components/prism-vim.js';
+// import 'prismjs/components/prism-csv.js';
+// import 'prismjs/components/prism-yaml.js';
+
 import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
 import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
--- src/utils/PostViewer/PostViewer.js
+++ src/utils/PostViewer/PostViewer.js
@@ -1,11 +1,49 @@
 import React from 'react';
 import './PostViewer.css';
+import Prism from 'prismjs';
 import { Viewer } from '@toast-ui/react-editor';
-import ReactMarkdown from 'react-markdown';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+
+import 'codemirror/lib/codemirror.css';
+import '@toast-ui/editor/dist/toastui-editor.css';
+
+import 'prismjs/components/prism-python.js';
+// import 'prismjs/themes/prism.css';
+import 'prismjs/components/prism-c.js';
+import 'prismjs/components/prism-cmake.js';
+import 'prismjs/components/prism-cpp';
+// import 'prismjs/components/prism-django.js';
+import 'prismjs/components/prism-docker.js';
+// import 'prismjs/components/prism-fortran.js';
+// import 'prismjs/components/prism-git.js';
+// import 'prismjs/components/prism-go.js';
+// import 'prismjs/components/prism-cshtml.js';
+import 'prismjs/components/prism-java.js';
+// import 'prismjs/components/prism-json.js';
+// import 'prismjs/components/prism-kotlin';
+// import 'prismjs/components/prism-log.js';
+import 'prismjs/components/prism-nginx.js';
+import 'prismjs/components/prism-ocaml.js';
+import 'prismjs/components/prism-php.js';
+// import 'prismjs/components/prism-regex.js';
+// import 'prismjs/components/prism-ruby.js';
+import 'prismjs/components/prism-sql.js';
+// import 'prismjs/components/prism-swift';
+import 'prismjs/components/prism-tsx.js';
+import 'prismjs/components/prism-vim.js';
+// import 'prismjs/components/prism-csv.js';
+// import 'prismjs/components/prism-yaml.js';
+
+import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
 
 function PostViewer({ content }) {
-  return <Viewer className="MyViewer" initialValue={content} />;
+  return (
+    <Viewer
+      className="MyViewer"
+      plugins={[[codeSyntaxHighlight, { highlighter: Prism }]]}
+      initialValue={content}
+    />
+  );
   // return (
   //   <ReactMarkdown
   //     children={content}
