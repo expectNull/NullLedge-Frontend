57397cdf13a38af5efd69f80e159c0e093605caa
LONGNEW <shinhyoung26@gmail.com>
2022-04-01 02:45:09 +0900

    modify : again... plz

--- src/utils/MyEditor/MyEditor.js
+++ src/utils/MyEditor/MyEditor.js
@@ -2,14 +2,11 @@ import React from 'react';
 import Prism from 'prismjs';
 import './MyEditor.css';
 
+import 'prismjs/themes/prism.css';
 import 'codemirror/lib/codemirror.css';
 import '@toast-ui/editor/dist/toastui-editor.css';
 
 import 'prismjs/components/prism-python.js';
-// import 'prismjs/themes/prism.css';
-import 'prismjs/components/prism-c.js';
-import 'prismjs/components/prism-cmake.js';
-import 'prismjs/components/prism-cpp';
 // import 'prismjs/components/prism-django.js';
 import 'prismjs/components/prism-docker.js';
 // import 'prismjs/components/prism-fortran.js';
@@ -27,11 +24,14 @@ import 'prismjs/components/prism-php.js';
 // import 'prismjs/components/prism-ruby.js';
 import 'prismjs/components/prism-sql.js';
 // import 'prismjs/components/prism-swift';
-import 'prismjs/components/prism-tsx.js';
-import 'prismjs/components/prism-vim.js';
+// import 'prismjs/components/prism-tsx.js';
+// import 'prismjs/components/prism-vim.js';
 // import 'prismjs/components/prism-csv.js';
 // import 'prismjs/components/prism-yaml.js';
 
+import 'codemirror/lib/codemirror.css';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import 'prismjs/components/prism-python.js';
 import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
 import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
--- src/utils/PostViewer/PostViewer.js
+++ src/utils/PostViewer/PostViewer.js
@@ -3,14 +3,11 @@ import './PostViewer.css';
 import Prism from 'prismjs';
 import { Viewer } from '@toast-ui/react-editor';
 
+import 'prismjs/themes/prism.css';
 import 'codemirror/lib/codemirror.css';
 import '@toast-ui/editor/dist/toastui-editor.css';
 
 import 'prismjs/components/prism-python.js';
-// import 'prismjs/themes/prism.css';
-import 'prismjs/components/prism-c.js';
-import 'prismjs/components/prism-cmake.js';
-import 'prismjs/components/prism-cpp';
 // import 'prismjs/components/prism-django.js';
 import 'prismjs/components/prism-docker.js';
 // import 'prismjs/components/prism-fortran.js';
@@ -28,8 +25,8 @@ import 'prismjs/components/prism-php.js';
 // import 'prismjs/components/prism-ruby.js';
 import 'prismjs/components/prism-sql.js';
 // import 'prismjs/components/prism-swift';
-import 'prismjs/components/prism-tsx.js';
-import 'prismjs/components/prism-vim.js';
+// import 'prismjs/components/prism-tsx.js';
+// import 'prismjs/components/prism-vim.js';
 // import 'prismjs/components/prism-csv.js';
 // import 'prismjs/components/prism-yaml.js';
 
@@ -40,34 +37,10 @@ function PostViewer({ content }) {
   return (
     <Viewer
       className="MyViewer"
-      plugins={[[codeSyntaxHighlight, { highlighter: Prism }]]}
+      plugins={[codeSyntaxHighlight, { highlighter: Prism }]}
       initialValue={content}
     />
   );
-  // return (
-  //   <ReactMarkdown
-  //     children={content}
-  //     className="MyViewer"
-  //     components={{
-  //       code({ node, inline, className, children, ...props }) {
-  //         const match = /language-(\w+)/.exec(className || '');
-  //         return !inline && match ? (
-  //           <SyntaxHighlighter
-  //             children={String(children).replace(/\n$/, '')}
-  //             // style={darcula}
-  //             language={match[1]}
-  //             PreTag="div"
-  //             {...props}
-  //           />
-  //         ) : (
-  //           <code className={className} {...props}>
-  //             {children}
-  //           </code>
-  //         );
-  //       },
-  //     }}
-  //   />
-  // );
 }
 
 export default PostViewer;
