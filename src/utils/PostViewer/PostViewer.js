21cabc677443cdfa6c8794f3374ce73f0c87a2c2
LONGNEW <shinhyoung26@gmail.com>
2022-04-01 00:14:43 +0900

    modify : for code highlight

--- src/utils/PostViewer/PostViewer.js
+++ src/utils/PostViewer/PostViewer.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import { Viewer } from '@toast-ui/react-editor';
 import './PostViewer.css';
+import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 
 function PostViewer({ content }) {
-  return <Viewer className="MyViewer" initialValue={content} />;
+  return (
+    <ReactMarkdown
+      children={content}
+      className="MyViewer"
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || '');
+          return !inline && match ? (
+            <SyntaxHighlighter
+              children={String(children).replace(/\n$/, '')}
+              // style={darcula}
+              language={match[1]}
+              PreTag="div"
+              {...props}
+            />
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    />
+  );
 }
 
 export default PostViewer;
